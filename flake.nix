8c5e57e7eab4002c0d1d9b33d4e94fdae49c9faf
Daniel-Railsback <drailsback@healthrise.com>
2025-06-05 19:09:58 -0600

    Added NVF support for Neovim builds

--- flake.nix
+++ flake.nix
@@ -3,17 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nvf.url = "github:notashelf/nvf";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
+  outputs = { nixpkgs, home-manager, nvf, ... }: {
     nixosConfigurations."TheoNixos" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./hosts/TheoNixos.nix
+	nvf.nixosModules.default
         home-manager.nixosModules.home-manager
         {
           home-manager.useUserPackages = true;
